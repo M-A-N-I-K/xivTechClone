6ce2233c1c7aa4e712f76318d390cf7481a692c5
Manik Dingra <manikdhingra0582@gmail.com>
2023-05-11 15:44:49 +0530

    Header made responsive and close icon added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Svg from "../assets/xivTech.svg"
+import Close from "../assets/close.svg"
 
 const header = () => {
     const [toggleServices, setToggleServices] = useState(false);
@@ -9,7 +10,7 @@ const header = () => {
         <>
             <nav className="bg-white sticky top-0 z-10 shadow-xl border-gray-200 dark:border-gray-600 dark:bg-gray-900">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <a href="#" className="flex h-12 w-12 self-end">
+                    <a href="#" className="z-30 flex h-12 w-12 self-end">
                         <img src={Svg} className='h-16 w-14 object-cover' alt="XivTech Logo" />
                         <div className='flex flex-col'>
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">XivTech
@@ -19,14 +20,19 @@ const header = () => {
                             </span>
                         </div>
                     </a>
-                    <button onClick={() => {
-                        setToggleMenu(!toggleMenu),
-                            setToggleServices(false)
-                    }} data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
+                    <button onClick={() =>
+                        setToggleMenu(true)
+                    } data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
-                    <div id="mega-menu-full" className={`items-center justify-between font-medium ${toggleMenu ? "absolute top-10 left-0" : "hidden"} md:relative md:top-0 w-full md:flex md:w-auto md:order-1`}>
+                    {
+                        toggleMenu &&
+                        <button onClick={() => { setToggleMenu(false), setToggleServices(false) }}>
+                            <svg on className='absolute z-30 cursor-pointer right-5 top-7' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="25px" height="25px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z" /></svg>
+                        </button>
+                    }
+                    <div id="mega-menu-full" className={`items-center justify-between font-medium ${toggleMenu ? "absolute top-0 left-0" : "hidden"} md:relative md:top-0 w-full md:flex md:w-auto md:order-1`}>
                         <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li className='self-center'>
                                 <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
@@ -54,7 +60,7 @@ const header = () => {
                 </div>
                 {
                     toggleServices &&
-                    <div id="mega-menu-full-dropdown" className="w-full absolute top-40 md:top-14 right-5 mt-1 border-gray-200 shadow-sm bg-gray-50 px-4 lg:px-0 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+                    <div id="mega-menu-full-dropdown" className="w-full absolute top-28 md:top-14 right-5 mt-1 border-gray-200 shadow-sm bg-gray-50 px-4 lg:px-0 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
                         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white md:grid-cols-2 md:px-6">
                             <ul>
                                 <li>
